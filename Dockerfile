7c86e77c3f32d5994493eb6b3aa223b12765b0ef
Patrik martinovský <p4km9y@xit.camp>
2017-08-07 17:27:06 +0200

    zoo 3.5.4-beta

--- Dockerfile
+++ Dockerfile
@@ -11,23 +11,28 @@ ARG ZK_CLIENT_HEAP
 ENV ZK_CLIENT_HEAP ${ZK_CLIENT_HEAP:-256}
 
 ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
-ENV ZK_VERSION 3.5.2-alpha
+ENV ZK_VERSION 3.5.3-beta
 ENV ZK_LEADER ""
-ENV ZK_SLEEP 0
+ENV ZK_SLEEP -1
 
+# beta bug: not a gzip archive: wget -O - ${current}/${ref} | gzip -dc | tar x -C /opt/ -f - && \
 RUN current=http://www.apache.org/dist/zookeeper/zookeeper-${ZK_VERSION} && \
     ref=`wget -qO - ${current} | sed -n 's/.*href="\(.*zookeeper-.*\..*gz\)".*/\1/p'` && \
-    wget -O - ${current}/${ref} | gzip -dc | tar x -C /opt/ -f - && \
+    wget -O - ${current}/${ref} | tar x -C /opt/ -f - && \
     dir=`ls /opt | grep zookeeper` && \
     ln -s /opt/${dir} /opt/zookeeper && \
     mkdir -p /opt/zookeeper/data && \
-    mkdir -p /opt/zookeeper/log
+    mkdir -p /opt/zookeeper/logs
 
+# skipacl=yes not "true" as one would expect
 RUN adduser --no-create-home --home /opt/zookeeper --system --disabled-password --disabled-login zookeeper && \
     cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg && \
     sed -i 's/^\(dataDir\)\s*=.*$/\1=\/opt\/zookeeper\/data/' /opt/zookeeper/conf/zoo.cfg && \
     sed -i 's/^\s*\(rm\s\+.*ZOOPIDFILE.*\)$/while ps -p \$\(cat "$ZOOPIDFILE"\) > \/dev\/null; do sleep 1; echo "waiting for process termination"; done;\1/' /opt/zookeeper/bin/zkServer.sh && \
     echo "standaloneEnabled=false" >> /opt/zookeeper/conf/zoo.cfg && \
+    echo "reconfigEnabled=true" >> /opt/zookeeper/conf/zoo.cfg && \
+    echo "quorumListenOnAllIPs=true" >> /opt/zookeeper/conf/zoo.cfg && \
+    echo "skipACL=yes" >> /opt/zookeeper/conf/zoo.cfg && \
     echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg && \
     chown -R zookeeper:root /opt/zookeeper/ && \
     chmod +x /opt/zookeeper/bin/*.sh
--- zk-init.sh
+++ zk-init.sh
@@ -7,38 +7,54 @@
 ZK=${ZK_LEADER:-$1}
 
 IPADDRESSES=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
-echo -e "going to resolve host addresses:\n${IPADDRESSES}"
 for ADDR in ${IPADDRESSES}; do
     HOST=`host ${ADDR} | grep -v found | cut -d\  -f 5 | cut -d. -f1`
     if [ -n "${HOST}" ]; then
         break
     fi
 done
-
 if [ -z "${HOST}" ]; then
   HOST="${ADDR}"
   echo "reverse dns lookup unsuccessful, using ip address"
 fi
-echo "zookeeper host: >${HOST}< ip address: >${ADDR}<"
+echo -e "host: name: ${HOST} ip address: {\n${IPADDRESSES}\n}"
 
+DYN=`ls /opt/zookeeper/conf/zoo.cfg.dynamic* | wc -w`
 MYID=1
 echo "zookeeper leader address: ${ZK}"
 cd /opt/zookeeper
-if [ -n "${ZK}" ]; then
-  echo "sleeping for ${ZK_SLEEP} secs to avoid parallel reconfiguration"
-  /bin/sleep ${ZK_SLEEP}
 
+if [ -n "${ZK}" ]; then
+  if [ "${ZK_SLEEP}" -lt 0 ]; then
+    ZK_SLEEP=`shuf -i3-30 -n1`
+  fi
+  echo "sleeping for ${ZK_SLEEP} secs to avoid parallel reconfiguration"
+  sleep ${ZK_SLEEP}
+
+  echo "awaken: getting leader configuration"
   output=`/opt/zookeeper/bin/zkCli.sh -server ${ZK}:2181 get /zookeeper/config | grep ^server | tr '[:space:]' '\n'`
   echo -e "server response: zookeeper config: {\n${output}\n}"
 
   if [ -z "${output}" ]; then
-    echo "leader communication error: exitting"
-    exit 10
+    echo "leader communication error: trying existing cfg"
+    if [ "${DYN}" -gt 1 ]; then
+      echo "existing config found: going to start server"
+      output="${HOST}" # just for condition to pass
+    else
+      echo "existing config not found: exitting"
+      exit 10
+    fi
   fi
 
-  MYID=`grep ${IPADDRESS} <<< "${output}" | cut -d= -f1 | cut -d. -f2`
+  MYID=`grep ${HOST} <<< "${output}" | cut -d= -f1 | cut -d. -f2`
   if [ -z "${MYID}" ]; then
-    MYID=`grep ${HOST} <<< "${output}" | cut -d= -f1 | cut -d. -f2`
+    for ADDR in ${IPADDRESSES}; do
+      MYID=`grep ${ADDR} <<< "${output}" | cut -d= -f1 | cut -d. -f2`
+      if [ -n "${MYID}" ]; then
+          echo "ip address found in leader configuration: id=${MYID}"
+          break
+      fi
+    done
     if [ -z "${MYID}" ]; then
       # extract all the zk-ids from the output
       declare -a id_list=()
@@ -64,22 +80,25 @@ if [ -n "${ZK}" ]; then
       /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${MYID}
 
       echo "starting server for reconfiguration"
-      ZOO_LOG_DIR=/opt/zookeeper/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start
+      ZOO_LOG_DIR=/opt/zookeeper/logs ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start
+      sleep 5
+
       echo "starting leader reconfiguration"
       /opt/zookeeper/bin/zkCli.sh -server ${ZK}:2181 reconfig -add "server.${MYID}=${HOST}:2888:3888:participant;2181"
       echo "stopping reconfigured server"
-      /opt/zookeeper/bin/zkServer.sh stop
-
+      ZOO_LOG_DIR=/opt/zookeeper/logs /opt/zookeeper/bin/zkServer.sh stop
     fi
   fi
 
   echo "starting server: ${MYID}"
-  ZOO_LOG_DIR=/opt/zookeeper/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
-  sleep 1000
+  ZOO_LOG_DIR=/opt/zookeeper/logs ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
 else
-  # seq numbers are appended to dynamic files anyway - its just for the 1. time
-  echo "server.${MYID}=${HOST}:2888:3888;2181" > /opt/zookeeper/conf/zoo.cfg.dynamic
-  /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+  if [ "${DYN}" -lt 2 ]; then
+    echo "initializing leader"
+    # incremental reconfiguration changes dnamic file suffix - so it counts just for the 1. run anyway
+    echo "server.${MYID}=${HOST}:2888:3888;2181" > /opt/zookeeper/conf/zoo.cfg.dynamic
+    /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${MYID}
+  fi
   echo "starting leading server"
-  ZOO_LOG_DIR=/opt/zookeeper/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
+  ZOO_LOG_DIR=/opt/zookeeper/logs ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
 fi
